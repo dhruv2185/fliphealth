f55816de4f034771a64d361373046637fc3faa33
beakbreak <namanbhagat1414@gmail.com>
2023-08-07 01:37:34 +0530

    third contract commit

--- contracts/doctor.sol
+++ contracts/doctor.sol
@@ -1,31 +1,29 @@
-pragma solidity ^0.5.9;
+pragma solidity ^0.8.0;
 import "./patient.sol";
 contract Doctor is patient{
-    modifier AuthorizedDoctor( uint _doctorId, uint _patientId) {
-        uint128[] memory p = accessList[uint64(_doctorId)];
-        bool auth= false;
-        for( uint i=0; i < p.length; i++)
-        {
-            if(p[i]== _patientId){
-                auth=true;
-            }
-        }
-        require(auth == true);
-        _;
+
+     function registerDoctor(
+        uint128 _abhaId,
+        uint128 _aadharId,
+        string memory _name,
+        uint16 _age,
+        uint64 _grNum,
+        uint64 _mobile,
+        string memory _email
+    ) public {
+        doctorIndex[msg.sender] = Doctor(
+            _abhaId,
+            _aadharId,
+            _name,
+            _age,
+            _grNum,
+            _mobile,
+            _email
+        );
     }
 
-    function registerDoctor(string memory _name, uint _abhaId, uint16 _age, string memory _degreePath) public {
-        uint64 index= uint64(doctors.length);
-        doctorIndex[msg.sender] = index;
-        doctors.push(Doctor(msg.sender, uint128(_abhaId), _name, _age, _degreePath));
-    }
-
-    function requestAccess (uint _patientId) public {
-        uint64 index= doctorIndex[msg.sender];
-        doctors[index].abhaId                 
-    } 
-
-    function viewDocuments() public view returns () {
+    function getHealthRecords(address patAddress) public view returns (HealthRecord[] memory) {
+        return userRecords[patAddress];
     }
 
 }
--- contracts/patient.sol
+++ contracts/patient.sol
@@ -1,54 +1,121 @@
-pragma solidity ^0.5.9;
+pragma solidity ^0.8.0;
 
 contract patient{
-    struct Patient{
-        address myAdd;
-        uint128 id; // will be replaced by abha id
-        uint128 id;
+    struct Patient {
+        uint128 abhaId; // will be replaced by abha id
+        uint128 aadharId;
         string name;
         uint16 age;
         string gender;
-        uint128 mobile;
+        uint64 mobile;
         string email;
     }
 
     struct Doctor {
-        address myAdd;
         uint128 abhaId; // will be replaced by abha id
+        uint128 aadharId;
         string name;
         uint16 age;
-        string degreePath;
-        uint128 mobile;
+        uint64 grNum;
+        uint64 mobile;
         string email;
     }
 
     struct HealthRecord {
+        string organisation;
+        string date;
+        string doctorName;
         string documentName;
-        uint64 documentId;
         string documentPath;
-        address owner;
         string documentCid;
-        // string timestamp;
+        address owner;
+        string documentType;
     }
-
 
     mapping(address => Patient) public patientIndex; // address of user to index in struct array
     mapping(address => Doctor) public doctorIndex; // address of user to index in struct array
     mapping(address => address[]) public accessList; // doctor id to user ids
     mapping(address => HealthRecord[]) public userRecords; // address of user to list od ids of health records
-    mapping(address => address) public requests; // patientid to doctorId
 
-    function register_patient(string memory _name, uint16 _age, uint _patientId, string memory _gender, uint _mobile, string memory _email) public {
-        patientIndex[msg.sender] = Patient(msg.sender, uint128(_patientId), _name, _age, _gender, uint128(_mobile), _email);
+    function isAuthorized(address user, address doctor)  internal view returns (bool) {
+        address[] memory authorizedUsers = accessList[doctor];
+        for (uint i = 0; i < authorizedUsers.length; i++) {
+            if (authorizedUsers[i] == user) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    function addRecordByUser (string memory _name, string memory _path, string memory _cid) public {
-        userRecords[msg.sender].push(HealthRecord(_name, healthRecordId, _path, msg.sender, _cid));
+    // Define a modifier to check if the user's address is in the access list for a specific doctor
+    // modifier onlyAuthorized(address doctor) {
+    //     require(isAuthorized(msg.sender, doctor), "Not authorized.");
+    //     _;
+    // }
+
+    function register_patient(
+        string memory _name,
+        uint16 _age,
+        uint _abhaId,
+        uint _aadharId,
+        string memory _gender,
+        uint _mobile,
+        string memory _email
+    ) public {
+        patientIndex[msg.sender] = Patient(
+            uint128(_abhaId),
+            uint128(_aadharId),
+            _name,
+            _age,
+            _gender,
+            uint64(_mobile),
+            _email
+        );
     }
 
-    function grantAccessToDoctor(uint _doctorId) public {
+    function addRecordByUser(string memory _org,
+        string memory _date,
+        string memory _docName,
+        string memory _name,
+        string memory _path,
+        string memory _cid,
+        string memory _docType
+    ) public {
+        userRecords[msg.sender].push(HealthRecord(_org, _date, _name, _docName, _path, _cid, msg.sender, _docType));
+    }
+
+
+
+    // modifier AuthorizedDoctor(address doctor, address patientAdd) {
+    //     address[] memory p = accessList[doctor];
+    //     bool auth= false;
+    //     for( uint i=0; i < p.length; i++)
+    //     {
+    //         if(p[i]== patientAdd){
+    //             auth=true;
+    //         }
+    //     }
+    //     require(auth == true, "unauthorized doctor");
+    //     _;
+    // }
+
+    function grantAccessToDoctor(address _doctorAddress) public {
         // check if requesting user is owner
         // if yes then add this document id to access list corresponding to doctor id
-        accessList[_doctorId].push(_patientId);                
+        accessList[_doctorAddress].push( msg.sender);
+    }
+
+    function revokeAccess(address _doctorAddress)  public{
+        require(isAuthorized(msg.sender, _doctorAddress), "unauthorized");
+        address[] storage addresses = accessList[msg.sender];
+        for( uint i=0; i < accessList[_doctorAddress].length; i++)
+        {
+            if(accessList[_doctorAddress][i]== msg.sender){
+                uint indexToDelete = i;
+                require(indexToDelete < addresses.length, "Element not found in the array");
+                addresses[indexToDelete] = addresses[addresses.length - 1];
+                addresses.pop();
+            }
+        }
     }
 }
