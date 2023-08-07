d8fad550cf581a66dae76b804c7e201451e83186
dhruv2185 <dhruvghevariya2002@gmail.com>
2023-08-07 18:31:11 +0530

    doctor data extraction

--- src/assets/signinflow/DoctorLogin.js
+++ src/assets/signinflow/DoctorLogin.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import Web3 from 'web3';
 import { useState } from 'react';
@@ -19,11 +19,15 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Footer from '../../components/Footer';
 
 const DoctorLogin = () => {
-    const [data, setdata] = useState({
-        address: "",
-        Balance: null,
-    });
     const [isLoading, setIsLoading] = useState(true);
+    const name = useRef();
+    const age = useRef();
+    const email = useRef();
+    const phone = useRef();
+    const abha = useRef();
+    const aadhar = useRef();
+    const grnumber = useRef();
+    const [accounts, setAccounts] = useState([]);
     useEffect(() => {
 
         // Asking if metamask is already present or not
@@ -31,7 +35,7 @@ const DoctorLogin = () => {
             window.ethereum
                 .request({ method: "eth_requestAccounts" })
                 .then((res) => {
-                    console.log(res);
+                    setAccounts(res);
                     setIsLoading(false)
                 });
         } else {
@@ -40,11 +44,8 @@ const DoctorLogin = () => {
     }, [])
     const handleSubmit = (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+        const data = { name: name.current.value, age: age.current.value, phone: phone.current.value, abha: abha.current.value, aadhar: aadhar.current.value, email: email.current.value, grnumber: grnumber.current.value };
+        console.log(data);
     };
 
     return (
@@ -78,6 +79,7 @@ const DoctorLogin = () => {
                                 label="Name"
                                 type="text"
                                 id="name"
+                                inputRef={name}
                             />
                                 <TextField
                                     margin="normal"
@@ -88,6 +90,7 @@ const DoctorLogin = () => {
                                     type="number"
                                     inputProps={{ inputMode: "numeric", min: "1", max: "120" }}
                                     id="age"
+                                    inputRef={age}
                                 />
                             </Box>
                             <TextField
@@ -98,6 +101,7 @@ const DoctorLogin = () => {
                                 label="E-mail"
                                 name="email"
                                 type='email'
+                                inputRef={email}
                             />
                             <TextField
                                 margin="normal"
@@ -107,6 +111,7 @@ const DoctorLogin = () => {
                                 label="Mobile Number"
                                 name="phone"
                                 type='text'
+                                inputRef={phone}
                             />
                             <TextField
                                 margin="normal"
@@ -115,7 +120,7 @@ const DoctorLogin = () => {
                                 id="abha"
                                 label="ABHA ID"
                                 name="abhaID"
-                                autoFocus
+                                inputRef={abha}
                             />
                             <TextField
                                 margin="normal"
@@ -125,6 +130,7 @@ const DoctorLogin = () => {
                                 label="AADHAR ID"
                                 type="text"
                                 id="aadhar"
+                                inputRef={aadhar}
                             />
                             <TextField
                                 margin="normal"
@@ -133,6 +139,7 @@ const DoctorLogin = () => {
                                 id="GR-number"
                                 label="General Registration (G.R.) Number"
                                 name="grnumber"
+                                inputRef={grnumber}
 
                             />
                             <FormControlLabel
