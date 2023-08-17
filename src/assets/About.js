8eaebde6b382484c10df1c2fa1baf6b4351e8b7b
dhruv2185 <dhruvghevariya2002@gmail.com>
2023-08-17 20:46:05 +0530

    proxy server

--- src/assets/About.js
+++ src/assets/About.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Container, Image, Card, Typography } from "@mui/material";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const About = () => {
+    const [teamMembers, setTeamMembers] = useState([
+        {
+            name: "John Doe",
+            imageUrl: "https://picsum.photos/200/200",
+            description:
+                "John is the founder and CEO of our company. He is a visionary leader with a passion for technology.",
+        },
+        {
+            name: "Jane Doe",
+            imageUrl: "https://picsum.photos/200/200",
+            description:
+                "Jane is the CTO of our company. She is a brilliant engineer with a knack for solving complex problems.",
+        },
+        {
+            name: "Peter Smith",
+            imageUrl: "https://picsum.photos/200/200",
+            description:
+                "Peter is the lead developer of our team. He is a talented engineer with a deep understanding of software development.",
+        },
+    ]);
+
     return (
         <>
             <Navbar />
-            <Footer />
-        </>
+            <Container>
+                <h1>About Us</h1>
+                <Card>
+
+                </Card>
+            </Container>
+            <Footer /></>
+
     );
+
 }
 
 export default About;
